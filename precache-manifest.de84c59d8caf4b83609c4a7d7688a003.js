5814452669f7d67b13c7b8f785ab106e74cb4ff8
Rudenko Artem <artem.rud@bk.ru>
2019-11-05 01:18:55 +0200

    Updates

---
+++ precache-manifest.de84c59d8caf4b83609c4a7d7688a003.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5496e78e4ebdb88c02363a672b519bf6",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "2751d6fe5861ee5296e1",
+    "url": "/react_dynamic-list-of-todos/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "f7b07008d33c48195755",
+    "url": "/react_dynamic-list-of-todos/static/js/2.5e1383c2.chunk.js"
+  },
+  {
+    "revision": "2751d6fe5861ee5296e1",
+    "url": "/react_dynamic-list-of-todos/static/js/main.c53f32dd.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
